7ff83c1ab1595fe44ebb3b2b0af599af4539b38e
MakerbakerYT <65385732+MakerbakerYT@users.noreply.github.com>
2020-08-08 00:37:29 -0600

    Create CustomEnchantIds.php

---
+++ scr/DaPigGuy/PiggyCustomEnchants/enchants/CustomEnchantIds.php
@@ -0,0 +1,154 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaPigGuy\PiggyCustomEnchants\enchants;
+
+class CustomEnchantIds
+{
+    /**
+     * Global
+     */
+	const LUCKY = 269;
+	const REFORGED = 243;
+
+    /**
+     * Weapons
+     */
+	const RAGE = 299;
+
+    /**
+     * Sword
+     */
+	const MASTERYSILENCED = 613;
+	const DEMONICLIFESTEAL = 610;
+	const DIVINEIMMOLATION = 502;
+	const SOLITUDE = 247;
+	const ICEASPECT = 271;
+	const ENRAGE = 267;
+	const PARALYZE = 215;
+	const BLIND = 255;
+	const DOMINATE = 256;
+	const VENOM = 250;
+	const LIFESTEAL = 251;
+	const FEATHERWEIGHT = 234;
+	const DEEPWOUNDS = 233;
+	const EPICNESS = 206;
+	const EXECUTE = 201;
+	const INSOMNIA = 229;
+	const SILENCED = 200;
+	const THUNDERINGBLOW = 228;
+	const OBLITERATE = 227;
+	const SKILLSWIPE = 232;
+	const TRAP = 236;
+
+    /**
+     * Bows
+     */
+	const AUTOAIM = 207;
+	const BOMBARDMENT = 208;
+	const BOUNTYHUNTER = 209;
+	const GRAPPLING = 210;
+	const HEADHUNTER = 211;
+	const HEALING = 212;
+	const MISSILE = 213;
+	const MOLOTOV = 214;
+	const PIERCING = 216;
+	const SHUFFLE = 217;
+	const VOLLEY = 218;
+    const BLAZE = 219;
+    const HOMING = 220;
+    const PORKIFIED = 221;
+    const WITHERSKULL = 222;
+
+    /**
+     * Tools
+     */
+	const AUTOSMELT = 205;
+	const OXYGENATE = 202;
+	const DRILLER = 225;
+	const HASTE = 226;
+
+    /**
+     * Pickaxes
+     */
+
+    /**
+     * Axes
+     */
+	const DEEPBLEED = 606;
+	const MIGHTYCLEAVE = 605;
+	const EXTREMEINSANITY = 600;
+	const ROGUE = 500;
+	const DEVOUR = 291;
+	const INSANITY = 276;
+	const BLESSED = 268;
+	const CLEAVE = 263;
+	const BLEED = 262;
+	const PUMMEL = 261;
+	const CONFUSION = 230;
+	const BERSERK = 235;
+
+    /**
+     * Shovels
+     */
+
+    /**
+     * Hoes
+     */
+	const MASTERINQUISITIVE = 611;
+	const INQUISITIVE = 238;
+
+    /**
+     * Armor
+     */
+	const GODLYOVERLOAD = 612;
+	const DIVINEENLIGHTED = 603;
+	const MEGAARMORED = 601;
+	const MEGATANK = 602;
+	const NATUREWRATH = 501;
+	const DODGE = 270;
+	const ARMORED = 264;
+	const ANGELIC = 254;
+	const WITHER = 252;
+	const POISON = 253;
+	const SHOCKWAVE = 245;
+	const VOODOO = 242;
+	const SHRINK = 223;
+	const GROW = 224;
+	const TANK = 265;
+	const TRICKSTER = 237;
+
+    /**
+     * Helmet
+     */
+	const DRUNK = 280;
+	const CLARITY = 281;
+	const DESTRUCTION = 274;
+	const IMPLANTS = 258;
+	const RESILIENCE = 246;
+	const AQUATIC = 204;
+	const GLOWING = 203;
+
+    /**
+     * Chestplate
+     */
+	const OVERLOAD = 292;
+	const CURSE = 231;
+
+    /**
+     * Leggings
+     */
+
+    /**
+     * Boots
+     */
+	const METAPHYSICAL = 260;
+	const ROCKETESCAPE = 251;
+	const SPRINGS = 240;
+	const GEARS = 241;
+
+    /**
+     * Compass
+     */
+}
